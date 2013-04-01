9742a001132f747e3a8ad8fbfe1a3b45ac3b7f58
Hugo Ribeira <hugoribeira@gmail.com>
2013-04-01 03:11:12 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JkSite::Application.config.secret_token = '14c2d5c1571d50cc3dfab29fc72b132eb3401665b9b7f85fcfa37480ac827ff61f6e1ff4c7aee793cfd24041fd47a882e27c77cff3088fb48d97f03dd66e8c45'
